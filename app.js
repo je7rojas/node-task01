98d679fdba19af137375643d689723a8f35b3348
je7rojas <je7rojas@gmail.com>
2022-03-03 02:25:00 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+// DATA
+
+//Routers
+
+//Utils
+const { sequelize } = require("./util/database");
+
+//Init express app
+const app = express();
+
+// 2** -> success
+// 3** -> miscellaneous
+// 4** -> client error
+// 5** -> server error
+
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      users: users
+    }
+  });
+});
+
+//conexion de base de datos
+sequelize
+  .authenticate()
+  .then(() => console.log("Database authenticated"))
+  .catch((err) => console.log(err));
+
+app.listen(4000, () => {
+  console.log("Express app running 2.0");
+});
